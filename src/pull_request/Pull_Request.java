923cf43ff800fc25a672e375c37ce29e04bab0aa
slemagonzalez <slemagonzalez@victoria19.danielcastelao.org>
2018-01-11 09:52:35 +0100

    Pull Request

---
+++ src/pull_request/Pull_Request.java
@@ -0,0 +1,11 @@
+package pull_request;
+
+public class Pull_Request {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Proyecto creado. Esperando un pull request.");
+        
+    }
+    
+}
